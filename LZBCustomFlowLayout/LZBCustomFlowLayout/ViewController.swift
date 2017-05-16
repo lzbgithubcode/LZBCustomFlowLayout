3aefa20eee632effe1429ef4d29488e5b915725f
lzbgithubcode <1835064412@qq.com>
2017-05-16 22:46:26 +0800

    init

---
+++ LZBCustomFlowLayout/LZBCustomFlowLayout/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LZBCustomFlowLayout
+//
+//  Created by zibin on 2017/5/16.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    fileprivate lazy var collectionView : UICollectionView = {
+    
+    }()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
+
